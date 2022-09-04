84bb92232a042b15ca864bbbbf766d0c20816680
jaysmitjadhav <jadhav.jaysmit@gmail.com>
2022-09-04 10:14:07 -0700

    created models

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+// use express.json to get data into json format
+app.use(express.json());
+
+// PORT
+const PORT = process.env.PORT || 5050;
+
+// Connect mongoDB
+
+
+// Add port
+app.listen(PORT, () => console.log("Server connected"));
