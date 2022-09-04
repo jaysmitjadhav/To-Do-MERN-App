e07a773283f0027afb8fd63193b685a44582a40c
jaysmitjadhav <jadhav.jaysmit@gmail.com>
2022-09-04 11:34:11 -0700

    created REST API

--- server/routes/todoItems.js
+++ server/routes/todoItems.js
@@ -17,5 +17,36 @@ router.post('/api/item', async (req, res) => {
     }
 })
 
+// get data from database
+router.get('/api/items', async (req, res) =>{ 
+    try{
+        const allTodoItems = await todoItemsModel.find({});
+        res.status(200).json(allTodoItems)
+    }catch(err){
+        res.json(err);
+    }
+})
+
+// update todo item from database using id
+router.put('/api/item/:id', async (req, res) => {
+    try{
+        const updateItem = await todoItemsModel.findByIdAndUpdate(req.params.id, {$set: req.body});
+        res.status(200).json('Item updated');
+    }catch(err){
+        res.json(err);
+    }
+})
+
+// delete todo item from database using id
+router.delete('/api/item/:id', async (req, res) => {
+    try{
+        const deleteItem = await todoItemsModel.findByIdAndDelete(req.params.id);
+        res.status(200).json('Item deleted');
+    }catch(err){
+        res.json(err);
+    }
+})
+
+
 // export router 
 module.exports = router;
